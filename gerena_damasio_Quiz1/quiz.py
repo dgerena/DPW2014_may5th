6909eb4702b911bf4feadbfb93f0a06267e4268d
Eli <DGerena@EYE-OF-SAURON.local>
2014-05-07 22:29:00 -0400

    commit for posperity
    Squaring looks right

--- gerena_damasio_Quiz1/quiz.py
+++ gerena_damasio_Quiz1/quiz.py
@@ -3,11 +3,11 @@ Damasio Eli Gerena IV
 Basic Python Syntax Quiz
 May 7 2014
 '''
-def calc_area(h,w):
+def calc_area(h,w):#calculating the area
     if h == w :
         print "The area of your square is " + str(h * w)
     else:
-        print "The area of your rectangle is " + str(h *w)
+        print "The area of your rectangle is " + str(h *w)#math good
 
 calc_area(int(raw_input("Height:")),int(raw_input("Width:")))
 
