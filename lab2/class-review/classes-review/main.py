3f6ea66620a454eb3ad0860c4f409bb003b1bb65
Eli <DGerena@EYE-OF-SAURON.local>
2014-05-12 18:23:09 -0400

    classes review

--- lab2/class-review/classes-review/main.py
+++ lab2/class-review/classes-review/main.py
@@ -19,6 +19,33 @@ import webapp2
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
+        yoda =Character()
+        yoda.name="Yoda"
+        yoda.age=900
+        yoda.gender="Male"
+        yoda.occupation="Jedi Master"
+
+        luke=Character()
+        luke.name="Luke Skywalker"
+        luke.gender="Male"
+        luke.age="24"
+        luke.occupation="Farmer"
+
+        leia=Character()
+        leia.name="Leia Organa"
+        leia.gender="Female"
+        leia.age=luke.age
+        leia.occupation="Princess"
+
+class Character(object):
+    def __init__(self):
+        self.name =""
+        self.age =0
+        self.occupation =""
+        self.gender=""
+
+    def print_info(self):
+        print self.name+" is a(n) "+self.occupation
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
