4ddff192859e0bcd86d2901dcd509ac6f5ba38ac
dgerena <dgerena@fullsail.edu>
2014-05-21 17:46:39 -0400

    screwed it up last night… now it works again.

--- beer/main.py
+++ beer/main.py
@@ -12,19 +12,20 @@ class MainHandler(webapp2.RequestHandler):
         view=FormPage()
         view.form_header="Open Weather Map Api App"
         view._css_url="css/main.css"
-        look=""
+        look="glassware"
         params=""
         # if there are url variables..
-        if self.request.GET:
-            code = self.request.GET['code']
-            url ="http://api.brewerydb.com/v2/"+look+"?"+params+"key=0a1a3d54ec43b25abe64eaaff2c12970"
-            req =urllib2.Request(url)
-            opener = urllib2.build_opener()
-            data=opener.open(req)
-            obj=json.load(data)
-            self.response.write(obj)
-        else:
-            print "doesnt work."
+        # if self.request.GET:
+        # code = self.request.GET[url]
+        url ="http://api.brewerydb.com/v2/"+look+"?"+params+"key=0a1a3d54ec43b25abe64eaaff2c12970"
+        req =urllib2.Request(url)
+        opener = urllib2.build_opener()
+        data=opener.open(req)
+        obj=json.load(data)
+        for i in obj:
+            self.response.write('<br />'+str(obj[i]))
+        # else:
+        #     print "doesnt work."
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
