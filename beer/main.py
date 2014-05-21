83461f2f1234a95f12594f9796dfe661f7706971
dgerena <dgerena@fullsail.edu>
2014-05-21 03:02:00 -0400

    thank god for friends that know what there doing.

--- beer/main.py
+++ beer/main.py
@@ -5,15 +5,25 @@
 import webapp2
 import json
 import urllib2
+from page import *
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        code = self.request.GET('code')
-        url ="http://api.brewerydb.com/v2/?key=0a1a3d54ec43b25abe64eaaff2c12970"
-        req =urllib2.request(url)
-        opener = urllib2.build_opener()
-        data=opener.open(req)
-        print data
+        view=FormPage()
+        view.form_header="Open Weather Map Api App"
+        view._css_url="css/main.css"
+        look=""
+        params=""
+        # if there are url variables..
+        if self.request.GET:
+            code = self.request.GET('code')
+            url ="http://api.brewerydb.com/v2/"+look+"?"+params+"key=0a1a3d54ec43b25abe64eaaff2c12970"
+            req =urllib2.request(url)
+            opener = urllib2.build_opener()
+            data=opener.open(req)
+            print req
+        else:
+            print "doesnt work."
 
         self.response.write('Hello world!')
 app = webapp2.WSGIApplication([
