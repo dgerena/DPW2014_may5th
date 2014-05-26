3eee1d9cd898d990c1062558018a4e09cf157447
dgerena <dgerena@fullsail.edu>
2014-05-26 16:17:59 -0400

    SOoooo…. my way wasn't working.
    I was going to have to make quite a few duplications of code. to make
    it work… had to get help to find a better way.

--- beer/main.py
+++ beer/main.py
@@ -9,55 +9,71 @@ from page import *
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        view = FormPage()
-        view.form_header = "Beer"
-        view._css_url="css/main.css"
-        # glassware section
-        glassObj=self.Glassware()
-        dataObj= self.data()
-        pint=5
-        # beer glass keys of note. totalResults,data[]<this will let you choose a result
-        BeerGlassSearch=self.BeerSearch(pint)
+        page=Page()
+        get=GetDo()
 
-        # BeerGlassSearch['data'][0] this wil return the first(array num 0) search result for a *beer *search of *pint
-        # available gives a obj that has a descript. and name stating weather year round or otherwise.
-        print BeerGlassSearch['data'][0]['available']
-        k=''
-        for i in glassObj['data']:
-            k+='<option href="/?'+str(i['name'])+' >'+str(i['name'])+'</option>'
-        h=''
-        for j in BeerGlassSearch['data']:
-            h+='<div>'+str(j)+'</div>'
-        view._inputs=k
-        self.response.write(str(view.print_out())+str(h))
+        page.index(get.GetGlass())
+        self.response.write(page.index(get.GetGlass()))
 
-
-
-    def Glassware(self):
-        look="glassware"
-        params=''
-        url ="http://api.brewerydb.com/v2/glassware?key=0a1a3d54ec43b25abe64eaaff2c12970"
-        req =urllib2.Request(url)
-        opener = urllib2.build_opener()
-        data=opener.open(req)
-        obj=json.load(data)
-        return obj
-
-    def data(self):
-        url ="http://api.brewerydb.com/v2/?key=0a1a3d54ec43b25abe64eaaff2c12970"
-        req =urllib2.Request(url)
-        opener = urllib2.build_opener()
-        data=opener.open(req)
-        obj=json.load(data)
-        return obj
-
-    def BeerSearch(self,glassId):
-        url ="http://api.brewerydb.com/v2/beers?key=0a1a3d54ec43b25abe64eaaff2c12970&glasswareId="+str(glassId)
-        req =urllib2.Request(url)
-        opener = urllib2.build_opener()
-        data=opener.open(req)
-        obj=json.load(data)
-        return obj
+# SOooo ya... i did it wrong again..... gdamnit....
+#         view = FormPage()
+#         view.form_header = "Beer"
+#         view._css_url="css/main.css"
+#         # glassware section
+#         glassObj=self.Glassware()
+#         dataObj= self.data()
+#         pint=5
+#         # beer glass keys of note. totalResults,data[]<this will let you choose a result
+#         BeerGlassSearch=self.BeerSearch(pint)
+#
+#         # BeerGlassSearch['data'][0] this wil return the first(array num 0) search result for a *beer *search of *pint
+#         # available gives a obj that has a descript. and name stating weather year round or otherwise.
+#         print BeerGlassSearch['data'][0]['available']
+#         k=''
+#         h=''
+#         for i in glassObj['data']:
+#             k+='<option href="/?'+str(i['name'])+' >'+str(i['name'])+'</option>'
+#         for j in BeerGlassSearch['data']:
+#             h+='<div>'+str(j)+'</div>'
+#         view._inputs=k
+#         self.response.write(str(view.print_out())+str(h))
+#
+#
+#
+#     def Glassware(self):
+#         look="glassware"
+#         params=''
+#         url ="http://api.brewerydb.com/v2/glassware?key=0a1a3d54ec43b25abe64eaaff2c12970"
+#         req =urllib2.Request(url)
+#         opener = urllib2.build_opener()
+#         data=opener.open(req)
+#         obj=json.load(data)
+#         return obj
+#
+#     def data(self):
+#         url ="http://api.brewerydb.com/v2/?key=0a1a3d54ec43b25abe64eaaff2c12970"
+#         req =urllib2.Request(url)
+#         opener = urllib2.build_opener()
+#         data=opener.open(req)
+#         obj=json.load(data)
+#         return obj
+#
+#     def BeerSearch(self,glassId):
+#         url ="http://api.brewerydb.com/v2/beers?key=0a1a3d54ec43b25abe64eaaff2c12970&glasswareId="+str(glassId)
+#         req =urllib2.Request(url)
+#         opener = urllib2.build_opener()
+#         data=opener.open(req)
+#         obj=json.load(data)
+#         return obj
+#
+#     #view -> controller-> model
+#     #
+#     #
+#
+#
+# class Model(object):
+#     def __init__(self):
+#         pass
 
 
 app = webapp2.WSGIApplication([
--- beer/page.py
+++ beer/page.py
@@ -1,70 +1,107 @@
-import webapp2
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
-
+import urllib2
+import json
 
 class Page(object):
     def __init__(self):
-        self._open='''
-<!Doctype html>
+        self.head="""<!Doctype html>
 <html>
     <head>
         <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" />
         <link rel="stylesheet" type="text/css" href="{self.css_url}" />
         <title>{self.title}</title>
     </head>
-    <body>'''
-        self._content=''
-        self._close='''
+    <body>
+"""
+        self.foot="""
     </body>
-</html>
-        '''
-        self._css_url=''
-        self._title=''
-        self.all=''
+</html>"""
+    def Body(self):
+        pass
 
-    @property
-    def title(self):
-        return self._title
+    def index(self,arr):
 
-    @title.setter
-    def title(self,t):
-        self._title=t
-    @property
-    def css_url(self):
-        return self._css_url
+        select = '<select>'
+        for obj in arr:
+            select = select + '<option value="'+str(obj['id'])+'">'+obj['name']+'</option>'
+        select = select + '</select>'
+        return self.head+select+self.foot
 
-    @css_url.setter
-    def css_url(self,c):
-        self._css_url=c
-
-    def print_out(self):
-        self.update()
-        return self.all
-
-    def update(self):
-        self.all = self._open + self._content + self._close
-        self.all = self.all.format(**locals())
-
-class FormPage(Page):
+class GetDo(object):
     def __init__(self):
-        #call the supers init
-        Page.__init__(self)
-        #alternate way is super(FormPage,self).__init__()
-        self.__form_open='<form method="get" action=""><select>'
-        #should use a apropriate loop to make inputs dynamic based off of given arguments.
-        self._inputs='''
-        '''
-        self.__form_close='</select class="btn"></form>'
-        self.form_header=">>Form Header<<"
-        self.page_content=''
-        self._content=self.form_header+self.__form_open+self._inputs+self._close
+        pass
 
-    def update(self):
-        self.all = self._open + self.form_header+self.__form_open+self._inputs+self.__form_close+self.page_content+self._close
-        self.all = self.all.format(**locals())
+    def Get(self,direct,query):
+        apiKey="0a1a3d54ec43b25abe64eaaff2c12970"
+        self.url ="http://api.brewerydb.com/v2/"+direct+"/?key="+apiKey+query
+        self.req =urllib2.Request(self.url)
+        self.opener = urllib2.build_opener()
+        self.data=self.opener.open(self.req)
+        self.obj=json.load(self.data)
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+    def GetGlass(self):
+        self.Get('glassware','')
+        return self.obj['data']
+
+# class Page(object):
+#     def __init__(self):
+#         self._open='''
+# <!Doctype html>
+# <html>
+#     <head>
+#         <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" />
+#         <link rel="stylesheet" type="text/css" href="{self.css_url}" />
+#         <title>{self.title}</title>
+#     </head>
+#     <body>'''
+#         self._content=''
+#         self._close='''
+#     </body>
+# </html>
+#         '''
+#         self._css_url=''
+#         self._title=''
+#         self.all=''
+#
+#     @property
+#     def title(self):
+#         return self._title
+#
+#     @title.setter
+#     def title(self,t):
+#         self._title=t
+#     @property
+#     def css_url(self):
+#         return self._css_url
+#
+#     @css_url.setter
+#     def css_url(self,c):
+#         self._css_url=c
+#
+#     def print_out(self):
+#         self.update()
+#         return self.all
+#
+#     def update(self):
+#         self.all = self._open + self._content + self._close
+#         self.all = self.all.format(**locals())
+#
+# class FormPage(Page):
+#     def __init__(self):
+#         #call the supers init
+#         Page.__init__(self)
+#         #alternate way is super(FormPage,self).__init__()
+#         self.__form_open='<form method="get" action=""><select>'
+#         #should use a apropriate loop to make inputs dynamic based off of given arguments.
+#         self._inputs='''
+#         '''
+#         self.__form_close='</select class="btn"></form>'
+#         self.form_header=">>Form Header<<"
+#         self.page_content=''
+#         self._content=self.form_header+self.__form_open+self._inputs+self._close
+#
+#     def update(self):
+#         self.all = self._open + self.form_header+self.__form_open+self._inputs+self.__form_close+self.page_content+self._close
+#         self.all = self.all.format(**locals())
+# app = webapp2.WSGIApplication([
+#     ('/', MainHandler)
+# ], debug=True)
